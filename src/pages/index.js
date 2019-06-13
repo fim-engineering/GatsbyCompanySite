229115dd0b8b3edb90e0e3195fc3f6c0c9cf5daf
ihsansatriawan <ihsan.satriawan.20@gmail.com>
2019-06-13 09:50:11 +0700

    test integrate with wordpress

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,21 +1,39 @@
 import React from "react"
-import { Link } from "gatsby"
+import { useStaticQuery, graphql, Link } from "gatsby"
 
 import Layout from "../components/layout"
 import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+const IndexPage = () => {
+
+  const { allWordpressPost } = useStaticQuery(
+    graphql`
+      {
+        allWordpressPost {
+          nodes {
+            id
+            date
+            title
+            content
+          }
+        }
+      }
+    `
+  )
+
+  return <Layout>
+      <div>
+      {
+        allWordpressPost.nodes.map(node => {
+          return <div key={node.id}>
+            <h1>{node.title}</h1>
+            <div dangerouslySetInnerHTML={{ __html: node.content }} ></div>
+          </div>
+        })
+      }
+      </div>
   </Layout>
-)
+}
 
 export default IndexPage
